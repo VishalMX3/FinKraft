21c7b668975feb7dac9439b80948c705c3e2a52c
Vishal Pundhir <f20191055@pilani.bits-pilani.ac.in>
2024-05-06 18:37:55 +0530

    Connected to MongoDB Database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,22 @@
 var express = require("express");
 var app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connection successful"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(cors());
+
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.send("Helloz!");
 });
 app.listen(5000, function () {
   console.log("Example app listening on port 5000!");
